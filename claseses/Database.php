ff1a7da430f4c036396fdbd51a7f953fa0e1b01a
andrey_dyadyura <woolfy80@gmail.com>
2017-02-15 22:08:12 +0200

    create database

--- claseses/Database.php
+++ claseses/Database.php
@@ -1,12 +1,23 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: woo
- * Date: 15.02.17
- * Time: 20:19
- */
 class Database
 {
+    private $db;
 
+    public function __construct()
+    {
+        $dsn = 'mysql:dbname=testdb;host=127.0.0.1';
+        $user = 'root';
+        $password = '111';
+        try {
+            $this->db = new PDO($dsn, $user, $password);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+
+        // Select target database
+        mysql_select_db($this->database, $this->connection)
+        or die(mysql_error());
+
+    }
 }
---
+++ init.sql
@@ -0,0 +1,29 @@
+CREATE TABLE blog.user
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  hash CHAR(32) NOT NULL
+);
+CREATE UNIQUE INDEX user_id_uindex ON blog.user (id);
+CREATE UNIQUE INDEX user_email_uindex ON blog.user (email);
+ALTER TABLE blog.user COMMENT = 'admins and black list functions';
+
+CREATE TABLE blog.post
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  content TEXT,
+  status ENUM("new", "open", "closed") DEFAULT "new" NOT NULL,
+  tags TEXT DEFAULT NULL
+);
+CREATE UNIQUE INDEX post_id_uindex ON blog.post (id);
+ALTER TABLE blog.post COMMENT = 'posts';
+
+CREATE TABLE blog.commnet
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  parent_id INT(11) NOT NULL,
+  content TEXT DEFAULT NULL,
+  email VARCHAR(255) NOT NULL
+);
+CREATE UNIQUE INDEX commnet_id_uindex ON blog.commnet (id);
