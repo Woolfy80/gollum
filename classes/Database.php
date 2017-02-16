10a0cd4926f3a064818ad8983c1a2a208ab54fe5
andrey_dyadyura <woolfy80@gmail.com>
2017-02-16 15:28:57 +0200

    sync to server for deploy; add editors area

--- classes/Database.php
+++ classes/Database.php
@@ -6,9 +6,10 @@ class Database
 
     public function __construct()
     {
-        $dsn = 'mysql:dbname=blog;host=localhost';
-        $user = 'root';
-        $password = '111';
+        $dsn = 'mysql:dbname='.DB_NAME.';host=localhost';
+        $user = 'woolfy';
+        $user = DB_USER;
+        $password = DB_PASS;
         try {
             $this->db = new PDO($dsn, $user, $password);
         } catch (PDOException $e) {
---
+++ editor/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once("../includes/config.php");
+
+    echo tag("h1", "Gollum blog editors aria");
+
+    try {
+        $posts = $db->db->query('SELECT * FROM blog_post ORDER BY id DESC')->fetchAll();
+        foreach ($posts as $key => $post) {
+            echo tag("div",
+                tag("h3",
+                    '<a href="view.php?id=' . $post['id'] . '">' .
+                    $post['title'] .
+                    '</a>'
+                )
+            );
+
+        }
+
+    } catch(PDOException $e) {
+            echo $e->getMessage();
+    }
+?>
